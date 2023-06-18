4101f9f9d9257bfc129a7a03916b7d1eca049105
César Alejandro Hernández Macías <up200775@alumnos.upa.edu.mx>
2023-06-17 18:40:16 -0600

    Version 1.0

---
+++ php/conexion.php
@@ -0,0 +1,20 @@
+<?php
+// Datos de conexión a la base de datos
+$servername = "localhost:3307";
+$username = "root";
+$password = "";
+$dbname = "gamespotlight";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+echo "Conexión exitosa";
+
+// Cerrar conexión
+$conn->close();
+?>
