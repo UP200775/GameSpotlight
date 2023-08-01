3b0e51faa678c87c222b93bb2a531e34e0a15e69
Gonzalo Guadalupe Gonzalez Alba <104162244+UP200420@users.noreply.github.com>
2023-08-01 17:05:54 -0600

    Cambios Gonza

---
+++ Staff/Yael.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<HTML lang="en">
+
+<head>
+    <meta charset="UTF=-8">
+    <META name="viewport" content="width=device-width, initial-scale = 1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie-edge">
+    <title>Yael</title> <!--este es un comentaro : sirve para popner nombre a la pagina-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../bootstrap-5.3.0-dist/css/bootstrap.min.css">  
+    <link rel="stylesheet" href="../Header/Header.css"> 
+    <link rel="stylesheet" href="../Css/trabajadores.css">
+    <link rel="stylesheet" href="../Footer/estilos.css">
+</head>
+
+<header>
+<?php require_once'../Header/Header.html'; ?>
+</header>
+
+<body >
+    <div class="trabajador">
+        <img src="../Imagenes/Staff/Yael2.jpeg" alt="Foto del Trabajador" width="150" height="150">
+        <h2>Fernando Yael De la Torre</h2>
+        <p>Edad: 20</p>
+        <p>Matricula: UP200994</p>
+        <p>Videojuego favorito: Free Fire </p>
+        <p>Puesto: Periodista</p>
+        <a href="Staff.php" class="btn">Volver a la Lista</a>
+      </div>
+</body>
+<footer>
+        <?php require_once'../Footer/footer.html'; ?>
+    </footer>
+</HTML>
